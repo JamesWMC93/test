80117139ff083ba6077ae8e21909d40551576952
tankhaihong <tankhaihong01@gmail.com>
2022-10-04 21:32:17 +0800

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 
 
 import { OptionComponent } from './component/option/option.component';
-<<<<<<< HEAD
 import { AppComponent } from './app.component';
 import { SigninComponent } from './component/signin/signin.component';
 import { LoginComponent } from './component/login/login.component';
 import { StuOptionComponent } from './component/stu-option/stu-option.component';
 
-=======
 import { RegisterComponent } from './component/register/register.component';
->>>>>>> aab808ac4a060a26653cfb8edd1acd73fe67a1b3
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-<<<<<<< HEAD
     SigninComponent,
     StuOptionComponent,
-    OptionComponent
-=======
     OptionComponent,
     RegisterComponent
->>>>>>> aab808ac4a060a26653cfb8edd1acd73fe67a1b3
   ],
   imports: [
     BrowserModule,
