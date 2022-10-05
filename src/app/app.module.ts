0888d98c97a41a6dd7869c5aa9ab0653df15ca17
James <s10743057@ems.tcust.edu.tw>
2022-10-05 22:49:17 +0800

    8th Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,43 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { AppComponent } from './app.component';
-import { LoginComponent } from './component/login/login.component';
 import { OptionComponent } from './component/option/option.component';
 import { RegisterComponent } from './component/register/register.component';
-=======
-import { RouterModule,Routes } from '@angular/router';
->>>>>>> a1fb5aa2bd3981a7f0e10b2a8f965661fce98a43
-=======
->>>>>>> 6ed086b3304d9718890910377cc387d0e502fa82
 
 
-import { OptionComponent } from './component/option/option.component';
 import { AppComponent } from './app.component';
-import { SigninComponent } from './component/signin/signin.component';
+import{ SigninComponent} from './component/signin/signin.component';
 import { LoginComponent } from './component/login/login.component';
 import { StuOptionComponent } from './component/stu-option/stu-option.component';
+import { CheckComponent } from './component/check/check.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     OptionComponent,
-    RegisterComponent
-=======
-    SigninComponent,
-    StuOptionComponent
->>>>>>> a1fb5aa2bd3981a7f0e10b2a8f965661fce98a43
-=======
+    RegisterComponent,
     SigninComponent,
     StuOptionComponent,
-    OptionComponent
->>>>>>> 6ed086b3304d9718890910377cc387d0e502fa82
+    CheckComponent
   ],
   imports: [
     BrowserModule,
