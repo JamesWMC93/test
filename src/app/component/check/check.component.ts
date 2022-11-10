23e7d713c0fcaa9ae0958ec46c6acbfb36674140
James <s10743057@ems.tcust.edu.tw>
2022-11-10 09:17:24 +0800

    Updated

--- src/app/component/check/check.component.ts
+++ src/app/component/check/check.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';//form
 @Component({
   selector: 'app-check',
   templateUrl: './check.component.html',
   styleUrls: ['./check.component.css']
 })
 export class CheckComponent implements OnInit {
+  public form:any;
+  posts: any = [];
+  constructor(private fb: FormBuilder) { 
+    this.form=this.fb.group({ 
+      contactnum:"",
+      name:"",
 
-  constructor() { }
+    });
+
+  }
+
+
+  onSubmit(f:any){
+    if(f.invalid){
+      alert("invalid")
+    }
+    else
+    {
+      console.log(f);
+      console.log(f.value);
+      alert("success")
+      
+    }
+    
+  }
 
   ngOnInit(): void {
   }
 
+  
 }
