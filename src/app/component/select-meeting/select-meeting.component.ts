20b4dd6529a610070947275e6a6aa1c7b3af1f95
tankhaihong <tankhaihong01@gmail.com>
2022-11-22 14:27:09 +0800

    odify seclect Meeting page

--- src/app/component/select-meeting/select-meeting.component.ts
+++ src/app/component/select-meeting/select-meeting.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { StudentService } from 'src/app/services/student.service';
+import { FormBuilder, FormControl } from '@angular/forms';//form
+import { __values } from 'tslib';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-select-meeting',
@@ -7,10 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectMeetingComponent implements OnInit {
   myimage: string = "OeZSbQUfFq.jpg";
+  posts: any = [];
+  form: any;
+  Mname: string = "";
 
-  constructor() { }
+  constructor(private productURL: StudentService, private fb: FormBuilder, private http: HttpClient,private router:Router) { 
+    this.form = this.fb.group({
+      Mname: new FormControl(this.Mname[0]),
+    });
+
+
+  }
+
+
+  onSubmit(f: any) {
+
+    if (f.invalid) {
+      alert("invalid")
+    }
+    else {
+      this.http.post("https://192.168.0.5/face/roll-call/test.php/", f)
+        .subscribe((result) => {
+          console.log("result", result);
+        })
+      console.log(f);
+      alert("success")
+      this.router.navigateByUrl('簽到');
+    }
+
+  }
 
   ngOnInit(): void {
+    this.productURL.getSignin()
+      .subscribe((response: any) => {
+        this.posts = response;
+        this.Mname = response.Mname;
+      });
   }
+
 
 }
