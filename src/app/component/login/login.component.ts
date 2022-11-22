b2427b7811c9964fb12918cd27a5969cdad02ebe
James <s10743057@ems.tcust.edu.tw>
2022-11-22 14:27:14 +0800

    update1122

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/app/services/student.service';
+import{FormBuilder , FormControl}from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,15 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public form:any;
+  posts: any = [];
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private productURL: StudentService) { 
+    this.form=this.fb.group({ 
+      name:"",
+      id:"",
+      pwd:""
+    });
 
+  }
+
+  onSubmit(f:any){
+    if(f.invalid){
+      alert("invalid")
+    }
+    else
+    {
+      console.log(f);
+      console.log(f.value);
+      alert("success")
+      
+    }
+    
+  }
 
   closewin()
   {
   window.open('', '_self', ''); window.close();
   }
   
+  ngOnInit(): void {
+    this.productURL.getActivity()
+    .subscribe((response: any) => {
+      this.posts = response;
+    });
+  }
+
+}
+
+
   // ShowTime() {
   //     var NowDate = new Date();
   //     var y = NowDate.getFullYear();
@@ -27,7 +61,3 @@ export class LoginComponent implements OnInit {
   //     setTimeout('ShowTime()', 1000);
   // }
 
-  ngOnInit(): void {
-  }
-
-}
