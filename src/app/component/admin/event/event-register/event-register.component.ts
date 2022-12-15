94eb275b3ee5a9edefa3b8c37210e8927561d71e
tankhaihong <tankhaihong01@gmail.com>
2022-12-15 08:58:08 +0800

    modify sigin page

--- src/app/component/admin/event/event-register/event-register.component.ts
+++ src/app/component/admin/event/event-register/event-register.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormGroupDirective, Validators, FormControl } from '@angular
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder } from '@angular/forms';//form
 import { StudentService } from 'src/app/services/student.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-register',
@@ -15,24 +16,24 @@ export class EventRegisterComponent implements OnInit {
   form: FormGroup;
   response: any;
 
-  constructor(private http: HttpClient, private studentURL:StudentService) {
+  constructor(private http: HttpClient, private studentURL: StudentService, private router: Router) {
   }
 
 
   ngOnInit(): void {
-    this.form=new FormGroup({
-      coor:new FormControl(null,Validators.required),
+    this.form = new FormGroup({
+      coor: new FormControl(null, Validators.required),
       name: new FormControl(null, Validators.required),
       start: new FormControl(null, Validators.required),
       date: new FormControl(null, Validators.required),
       place: new FormControl(null, Validators.required),
       end: new FormControl(null, Validators.required),
       numPpl: new FormControl(null, Validators.required),
-      
+
     });
   }
 
-  onSubmit(form:any){
+  onSubmit(form: any) {
     const formData: FormData = new FormData()
     formData.append('name', form.name)
     formData.append('place', form.place)
@@ -41,17 +42,25 @@ export class EventRegisterComponent implements OnInit {
     formData.append('coor', form.coor)
     formData.append('end', form.end)
     formData.append('numPpl', form.numPpl)
-   
-    this.studentURL.onKaiXian(formData).subscribe((res: any) => {
-      console.log(res)
-      this.response = res;
-      alert("success")
-      // this.router.navigateByUrl('signin');
-    },
-      (err: any) => {
-        console.log(err)
-      }
-    )
+
+    if (form.invalid) {
+      alert("資料不完整")
+    }
+    else 
+    {
+      this.router.navigateByUrl('EventRegisterComponent');
+      this.studentURL.onKaiXian(formData).subscribe((res: any) => {
+        console.log(res)
+        this.response = res;
+        alert(res)
+
+      },
+        (err: any) => {
+          console.log(err)
+        }
+      )
+    }
+
 
   }
 
